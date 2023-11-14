89b75d90d3ed19b0f8cfac62ada26e056d724702
OQILOV2005 <u9217589@gmail.com>
2023-11-14 10:13:16 +0500

    Bu yerga o'zgartirishlaringiz haqida qisqacha izoh

--- home/settings.py
+++ home/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,6 +81,18 @@ DATABASES = {
     }
 }
 
+# DATABASES ={
+#
+# 'default': {
+#     'ENGINE': 'django.db.backends.postgresql',
+#     'NAME': 'railway',
+#     'USER': 'postgres',
+#     'PASSWORD':'uChVQyiBOeZOTSlLql1f',
+#     'HOST': 'containers-us-west-196.railway.app',
+#     'PORT':'6625'
+#
+# }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -117,7 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATICFILES_DIRS = os.path.join(BASE_DIR / 'static/'),
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles_build', 'static')
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
